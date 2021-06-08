f076bfbebedd3f7252a2f92051a9a98686c2a49c
nagarajuthandu <nagaraju.thandu@cmritonline.ac.in>
2021-06-08 11:49:27 +0530

    data binding 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Simple Angular APP';
+  numberA:number=5;
+  numberB:number=5;
 }
+
+
+
